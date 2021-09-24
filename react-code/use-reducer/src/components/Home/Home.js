4335a8a11d0e8013a3d828edeab2c2f03596ade7
chrishibby75 <chrishibby75@gmail.com>
2021-09-24 01:03:31 -0600

    learning new hooks... kind of

---
+++ react-code/use-reducer/src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import Card from '../Card/Card';
+import Button from "../Button/Button";
+import classes from './Home.module.css';
+import AuthContext from '../store/auth-context';
+
+const Home = () => {
+  const authCtx = useContext(AuthContext);
+  return (
+    <Card className={classes.home}>
+      <h1>Welcome back!</h1>
+      <Button onClick={authCtx.onLogout}>Logout</Button>
+    </Card>
+  );
+};
+
+export default Home;
